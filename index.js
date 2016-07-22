769c56a98a2052a48c7eb1e89e06c3ac0548a529
Tom Fagerland <tom@fagerland.org>
2016-07-22 16:04:51 +0200

    Clean up code, putting core functionality in function.

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 #! /usr/bin/env node
 
+function calculate (amount) {
+  let currency = '$';
+  if (amount.includes('kr')) {
+    currency = 'kr';
+  }
+  amount = amount.replace(currency, '');
+  if (isNaN(amount)) {
+    console.log('Please use numbers only');
+    process.exit(1);
+  }
+  let thirty = (currency === 'kr' ? 2.8 : 0.3);
+  let percentage = (currency === 'kr' ? (amount / 1000) * 34 : (amount / 1000) * 29);
+  let fee = thirty + percentage;
+  let result = amount - fee;
+  return {currency, fee, result};
+}
+function usage () {
+  console.log("Supply a price in either USD or NOK:\n\n> ppf '$220'\n> ppf '220k'\n\nNote that the using the format 'ppf $220' without quotation marks will not work due to constraints out of this developers control.");
+}
 let amount = process.argv[2];
-let currency = '$';
-if(amount.includes('kr')){
-  currency = 'kr';
+if (!amount) {
+  usage();
+  process.exit(0);
 }
-amount = amount.replace(currency,'');
-if(isNaN(amount)){
-  let e = new TypeError("Please use numbers only")
-}
-let thirty = (currency === 'kr' ? 2.8 : 0.3);
-let percentage = (currency === 'kr' ? (amount/1000)*34 : (amount/1000)*29);
-let fee = thirty + percentage;
-let result = amount - fee;
-console.log("Fee:\t"+currency+fee.toFixed(2)+"\nNet:\t"+currency+result.toFixed(2));
+let data = calculate(amount);
+console.log('Fee:\t' + data.currency + data.fee.toFixed(2) + '\nNet:\t' + data.currency + data.result.toFixed(2));
